199783ad0dfd47e9310eafc08f09132aba9daa0e
rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2026-03-01 00:28:58 +0600

    fix: correct import paths for Navbar and Footer components

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Epilogue, Inter } from "next/font/google";
 import "./globals.css";
-import { Navbar } from "./components/Navbar";
-import { Footer } from "./components/Footer";
+import { Navbar } from "../components/Navbar";
+import { Footer } from "../components/Footer";
 
 const epilogue = Epilogue({
   variable: "--font-epilogue",
